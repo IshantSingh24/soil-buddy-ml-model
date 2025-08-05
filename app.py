43cda5e03242484f475ad2a2774d2483bb6e3527
IshantSingh24 <ishant24singh@gmail.com>
2025-08-05 23:00:20 +0530

    flask

--- app.py
+++ app.py
@@ -1,21 +1,42 @@
-import streamlit as st
-import tensorflow as tf
-import numpy as np
+# import streamlit as st
+# import tensorflow as tf
+# import numpy as np
+# from PIL import Image
+
+# # Load model
+# model = tf.keras.models.load_model(r'soil_model.keras')  
+
+# CLASS_NAMES = ['Black Soil', 'Cinder Soil', 'Laterite Soil', 'Peat Soil', 'Yellow Soil']
+
+# st.title("Soil Type Classifier")
+
+# file = st.file_uploader("Upload an image", type=['jpg', 'jpeg', 'png'])
+
+# if file:
+#     img = Image.open(file).convert('RGB').resize((224, 224))
+#     st.image(img, width=224)
+#     x = np.expand_dims(np.array(img)/255.0, axis=0)
+#     pred = model.predict(x)
+#     pred_class = CLASS_NAMES[np.argmax(pred)]
+#     st.write("Prediction:", pred_class)
+from flask import Flask, request, jsonify
 from PIL import Image
+import numpy as np
+import tensorflow as tf
+from io import BytesIO
 
-# Load model
-model = tf.keras.models.load_model(r'soil_model.keras')  
-
+app = Flask(__name__)
+model = tf.keras.models.load_model("soil_model.keras")
 CLASS_NAMES = ['Black Soil', 'Cinder Soil', 'Laterite Soil', 'Peat Soil', 'Yellow Soil']
 
-st.title("Soil Type Classifier")
+@app.route("/predict", methods=["POST"])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file"}), 400
 
-file = st.file_uploader("Upload an image", type=['jpg', 'jpeg', 'png'])
-
-if file:
-    img = Image.open(file).convert('RGB').resize((224, 224))
-    st.image(img, width=224)
-    x = np.expand_dims(np.array(img)/255.0, axis=0)
+    file = request.files['file']
+    img = Image.open(file).convert("RGB").resize((224, 224))
+    x = np.expand_dims(np.array(img) / 255.0, axis=0)
     pred = model.predict(x)
     pred_class = CLASS_NAMES[np.argmax(pred)]
-    st.write("Prediction:", pred_class)
+    return jsonify({"prediction": pred_class})
